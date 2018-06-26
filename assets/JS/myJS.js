f261ae044b4f74eeaee5e01efca013f312a6a251
yuxin <yuxinhe2016@gmail.com>
2018-06-26 05:02:24 -0400

    playlist

--- assets/JS/myJS.js
+++ assets/JS/myJS.js
@@ -1,5 +1,93 @@
 $(document).ready(function() {
 
+	var player = videojs("video");
+	// Player will play the first item in myList
+	var myList = [{
+	  name: 'P!nk - Just Give Me A Reason ft. Nate Ruess',
+	  description: '',
+	  duration: 243,
+	  sources: [
+	    { src: 'https://www.youtube.com/embed/OpQFFLBMEPI', type: 'video/youtube' },
+	  ],
+	  // thumbnail give the pic in the playlist
+	  thumbnail: [
+	    {
+	      srcset: 'https://i.ytimg.com/vi/OpQFFLBMEPI/mqdefault.jpg',
+	      type: 'image/jpg',
+	      media: '(min-width: 400px;)'
+	    }
+	  ]
+	},
+	//second song in playlist
+	{
+	  name: 'P!nk - Try',
+	  description: '',
+	  duration: 249,
+	  sources: [
+	    { src: 'https://www.youtube.com/embed/yTCDVfMz15M', type: 'video/youtube' },
+	  ],
+	  thumbnail: [
+	    {
+	      srcset: 'https://i.ytimg.com/vi/yTCDVfMz15M/mqdefault.jpg',
+	      type: 'image/jpg',
+	      media: '(min-width: 400px;)'
+	    },
+	  ]
+	},
+	//3rd song in playlist
+	{
+	  name: 'P!nk - What About Us',
+	  description: '',
+	  duration: 321,
+	  sources: [
+	    { src: 'https://www.youtube.com/embed/ClU3fctbGls', type: 'video/youtube' },
+	  ],
+	  thumbnail: [
+	    {
+	      srcset: 'https://i.ytimg.com/vi/ClU3fctbGls/mqdefault.jpg',
+	      type: 'image/jpg',
+	      media: '(min-width: 400px;)'
+	    },
+	  ]
+	 },
+	 //4th song in playlist
+	{
+	  name: 'P!nk - Beautiful Trauma',
+	  description: '',
+	  duration: 297,
+	  sources: [
+	    { src: 'https://www.youtube.com/embed/EBt_88nxG4c', type: 'video/youtube' },
+	  ],
+	  thumbnail: [
+	    {
+	      srcset: 'https://i.ytimg.com/vi/EBt_88nxG4c/mqdefault.jpg',
+	      type: 'image/jpg',
+	      media: '(min-width: 400px;)'
+	    },
+	  ]
+	 },
+	 //5th song in playlist
+	{
+	  name: 'P!nk - Just Like Fire',
+	  description: '',
+	  duration: 232,
+	  sources: [
+	    { src: 'https://www.youtube.com/embed/5Nrv5teMc9Y', type: 'video/youtube' },
+	  ],
+	  thumbnail: [
+	    {
+	      srcset: 'https://i.ytimg.com/vi/5Nrv5teMc9Y/mqdefault.jpg',
+	      type: 'image/jpg',
+	      media: '(min-width: 400px;)'
+	    },
+	  ]
+	 }	
+	];
+
+	player.playlist(myList);
+	// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+	player.playlistUi();
+
 	var q = "";
 
 	var videoId = "";
@@ -34,7 +122,8 @@ $(document).ready(function() {
 	        			"&q=" + q +
 	        			"&maxResults=5" +
 	        			"&videoCategoryId=10" +
-	        			"&key=" + youTuBeApiKey;
+	        			"&key=" + 
+	        			youTuBeApiKey ;
 	        
 	    // Performing an AJAX request with the queryURL
 	    $.ajax({
@@ -44,15 +133,12 @@ $(document).ready(function() {
 	    	console.log(response);
 	    	videoId = response.items[0].id.videoId
 	    	var url= "https://www.youtube.com/embed/" + videoId;
-	    	//"<source src='"+url+"'>");
-	    	console.log(url);	
-
+	    	
 	    	//change the video ULR whenever the user type in keyword
 	    	videojs('preview-player').ready(function() {
 			  var myPlayer = this;
 			  myPlayer.src({ type: 'video/youtube', src: url });
 			});
-
 	    	
 		    var str = response.items[0].snippet.title;
 		    var res = str.split("-");
@@ -66,12 +152,36 @@ $(document).ready(function() {
 		    console.log(artistName);
 		    console.log(trackName);
 
+		    var durationURL = 	"https://www.googleapis.com/youtube/v3/videos?" +
+		    					"&part=&contentDetails" + youTuBeApiKey ;
+
+		   	console.log(durationURL);
+
+
+		    // myList[0].name = str ;
+		    // myList[0].duration = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//youTube ajax ends
 		}); 							
-
-	});	
-	
-
+	// #searchButton event function ends
+	});		
+//doc ready function ends
 });
+
+ 
 
 
 
